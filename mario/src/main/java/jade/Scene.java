813b0d0c8785f0af171d602f40e7d29ff9fa8988
saisesai <saisesai@qq.com>
2021-01-16 12:28:23 +0800

    20210116

---
+++ mario/src/main/java/jade/Scene.java
@@ -0,0 +1,17 @@
+package jade;
+
+public abstract class Scene {
+
+    protected Camera camera;
+
+    public Scene() {
+
+    }
+
+    public abstract void update(float dt);
+
+    public void init() {
+
+    }
+
+}
